052f658871635e996e51d25754eeea726d3e42f6
Barış Gündüz <baris.gunduz@devstup.org>
2021-09-21 11:01:04 +0300

    nyt news api

---
+++ src/components/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <footer
+    class="px-4 py-8 text-sm font-bold text-center text-white bg-black">
+    <p class="text-sm tracking-wide">Copyright (c) 2021 Devstup</p>
+  </footer>
+</template>
